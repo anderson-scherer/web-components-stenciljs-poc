a474de541ebff254b1b833a065199f1969afd149
Anderson Scherer <anderson.scherer.costa@gmail.comm>
2019-12-05 12:37:05 -0300

    Adicionando testes uniátios ao componente

---
+++ web-components/src/components/hello-world/hello-world.e2e.ts
@@ -0,0 +1,19 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('my-component', () => {
+  it('renders', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<hello-world></hello-world>');
+    const element = await page.find('hello-world');
+    expect(element).toHaveClass('hydrated');
+  });
+
+  it('renders changes to the name data', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<hello-world></hello-world>');
+    const element = await page.find('hello-world >>> div');
+    expect(element.textContent).toEqual(`My first Web Component`);
+  });
+});
