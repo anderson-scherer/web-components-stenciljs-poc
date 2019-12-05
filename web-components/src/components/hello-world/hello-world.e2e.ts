dd78b8e6ebcf55b7fa52f161303b9cebe4625fb2
Anderson Scherer <anderson.scherer.costa@gmail.comm>
2019-12-05 13:41:26 -0300

    Ajustes na camada de testes

--- web-components/src/components/hello-world/hello-world.e2e.ts
+++ web-components/src/components/hello-world/hello-world.e2e.ts
@@ -1,6 +1,6 @@
 import { newE2EPage } from '@stencil/core/testing';
 
-describe('my-component', () => {
+describe('hello-world', () => {
   it('renders', async () => {
     const page = await newE2EPage();
 
